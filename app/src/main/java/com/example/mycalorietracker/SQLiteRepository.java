41c02a857fc0784a05f5d65f9ffde8c5b1e30782
Piotr Chuchla <piotrchuchla9@gmail.com>
2022-01-15 16:25:35 +0100

    list

--- app/src/main/java/com/example/mycalorietracker/SQLiteRepository.java
+++ app/src/main/java/com/example/mycalorietracker/SQLiteRepository.java
@@ -1,4 +1,24 @@
 package com.example.mycalorietracker;
 
 public class SQLiteRepository implements DBRepository {
+
+
+
+
+
+
+    @Override
+    public Meal getMealByDay(int dayId) {
+        return null;
+    }
+
+    @Override
+    public Day getDay(int dayId) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(int productId) {
+        return null;
+    }
 }
