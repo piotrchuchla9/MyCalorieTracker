1e5fb924321318034fed63dc21d3173115385198
Piotr Chuchla <piotrchuchla9@gmail.com>
2022-01-27 00:37:15 +0100

    taking photos works

--- app/src/main/java/com/example/mycalorietracker/PhotoActivity.java
+++ app/src/main/java/com/example/mycalorietracker/PhotoActivity.java
@@ -1,25 +1,44 @@
 package com.example.mycalorietracker;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ActivityNotFoundException;
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.icu.text.CaseMap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class PhotoActivity extends AppCompatActivity {
 
-    ImageView myPhoto;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    public static final int CAMERA_ACTION_CODE = 1;
 
-    TextView noPhoto;
+    private ImageView myPhoto;
+    private TextView noPhoto;
+    private Button makePhoto;
 
-    Button makePhoto;
+    Uri image_uri;
 
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +52,7 @@ public class PhotoActivity extends AppCompatActivity {
 
         myPhoto = (ImageView) findViewById(R.id.myPhoto);
         noPhoto = (TextView) findViewById(R.id.noPhoto);
+        makePhoto = (Button) findViewById(R.id.makePhoto);
 
         if(myPhoto.getDrawable() == null) {
             noPhoto.setVisibility(View.VISIBLE);
@@ -40,7 +60,71 @@ public class PhotoActivity extends AppCompatActivity {
             noPhoto.setVisibility(View.INVISIBLE);
         }
 
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle bundle = result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) bundle.get("data");
+                        myPhoto.setImageBitmap(bitmap);
+                    }
+            }
+        });
+
+        makePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                                requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        openCamera();
+                    }
+                }
+                else {
+                    openCamera();
+                }
+            }
+        });
     }
 
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+//        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+        activityResultLauncher.launch(cameraIntent);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            myPhoto.setImageURI(image_uri);
+        }
+    }
 }
