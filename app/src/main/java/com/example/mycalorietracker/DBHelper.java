6e1e6fe842c3ab183e53cda616203818cbbba6e9
Piotr Korgol <piotr.korgol@gmail.com>
2022-01-30 22:13:20 +0100

    adding methods

--- app/src/main/java/com/example/mycalorietracker/DBHelper.java
+++ app/src/main/java/com/example/mycalorietracker/DBHelper.java
@@ -34,12 +34,21 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DAY_ID = "id";
     public static final String DAY_DAY = "day";
 
-
+    private static DBHelper sInstance;
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
+    public static synchronized DBHelper getInstance(Context context) {
+
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+        if (sInstance == null) {
+            sInstance = new DBHelper(context.getApplicationContext());
+        }
+        return sInstance;
+    }
 
 
 
@@ -48,33 +57,38 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE " + PRODUCTS_TABLE + " (id INTEGER PRIMARY KEY AUTOINCREMENT, productName TEXT, calories REAL, proteins REAL, carbs REAL, fats REAL)");
         db.execSQL("CREATE TABLE " + MEALS_TABLE + " (id INTEGER PRIMARY KEY AUTOINCREMENT, productId INTEGER, productName TEXT, calories REAL, proteins REAL, carbs REAL, fats REAL, quantity REAL, dayId INTEGER, FOREIGN KEY(productId) REFERENCES Products(id), FOREIGN KEY(dayId) REFERENCES Days(id))");
         db.execSQL("CREATE TABLE " + DAYS_TABLE + " (id INTEGER PRIMARY KEY AUTOINCREMENT, day TEXT)");
+
+        String product1 = "INSERT INTO " + PRODUCTS_TABLE + " (" + PRODUCTS_PRODUCT_NAME + ", " + PRODUCTS_CALORIES + ", " + PRODUCTS_PROTEINS + ", " + PRODUCTS_CARBS + ", " + PRODUCTS_FATS + ") VALUES ('Egg', '63', '5.5', '0.3', '4.2')";
+        String product2 = "INSERT INTO " + PRODUCTS_TABLE + " (" + PRODUCTS_PRODUCT_NAME + ", " + PRODUCTS_CALORIES + ", " + PRODUCTS_PROTEINS + ", " + PRODUCTS_CARBS + ", " + PRODUCTS_FATS + ") VALUES ('White Bread', '50', '1.5', '0.8', '1')";
+        String product3 = "INSERT INTO " + PRODUCTS_TABLE + " (" + PRODUCTS_PRODUCT_NAME + ", " + PRODUCTS_CALORIES + ", " + PRODUCTS_PROTEINS + ", " + PRODUCTS_CARBS + ", " + PRODUCTS_FATS + ") VALUES ('Rice', '340.6', '6', '78', '0.8')";
+
+        db.execSQL(product1);
+        db.execSQL(product2);
+        db.execSQL(product3);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        SQLiteDatabase.openDatabase("mydb",null, SQLiteDatabase.OPEN_READWRITE);
         db.execSQL("DROP TABLE IF EXISTS " + PRODUCTS_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + MEALS_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + DAYS_TABLE);
         onCreate(db);
     }
 
-    public boolean insertProductData(String productName, double caloreis, double proteins, double carbs, double fats) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(PRODUCTS_PRODUCT_NAME, productName);
-        contentValues.put(PRODUCTS_CALORIES, caloreis);
-        contentValues.put(PRODUCTS_PROTEINS, proteins);
-        contentValues.put(PRODUCTS_CARBS, carbs);
-        contentValues.put(PRODUCTS_FATS, fats);
-        long result = db.insert(PRODUCTS_TABLE, null, contentValues);
-        if(result == -1) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+//    public long insertProductData(String productName, double caloreis, double proteins, double carbs, double fats) {
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(PRODUCTS_PRODUCT_NAME, productName);
+//        contentValues.put(PRODUCTS_CALORIES, caloreis);
+//        contentValues.put(PRODUCTS_PROTEINS, proteins);
+//        contentValues.put(PRODUCTS_CARBS, carbs);
+//        contentValues.put(PRODUCTS_FATS, fats);
+//        long result = db.insert(PRODUCTS_TABLE, null, contentValues);
+//        return result;
+//    }
 
-    public boolean insertMealData(int productId, String productName, double caloreis, double proteins, double carbs, double fats, double quantity, int dayId) {
+    public long insertMealData(int productId, String productName, double caloreis, double proteins, double carbs, double fats, double quantity, int dayId) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(MEAL_PRODUCT_ID, productId);
@@ -86,23 +100,15 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put(MEAL_QUANTITY, quantity);
         contentValues.put(MEAL_DAY_ID, dayId);
         long result = db.insert(MEALS_TABLE, null, contentValues);
-        if(result == -1) {
-            return false;
-        } else {
-            return true;
-        }
+        return result;
     }
 
-    public boolean insertDayData(String day) {
+    public long insertDayData(String day) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(DAY_ID, day);
+        contentValues.put(DAY_DAY, day);
         long result = db.insert(DAYS_TABLE, null, contentValues);
-        if(result == -1) {
-            return false;
-        } else {
-            return true;
-        }
+        return result;
     }
 
 
--- app/src/main/java/com/example/mycalorietracker/SQLiteRepository.java
+++ app/src/main/java/com/example/mycalorietracker/SQLiteRepository.java
@@ -1,9 +1,19 @@
 package com.example.mycalorietracker;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLiteRepository implements DBRepository {
 
+    private final DBHelper helper;
+
+    public SQLiteRepository(Context context) {
+        helper = DBHelper.getInstance(context);
+    }
 
     @Override
     public Meal getMealByDay(int dayId) {
@@ -32,17 +42,30 @@ public class SQLiteRepository implements DBRepository {
 
     @Override
     public List<Day> getDays() {
-        return null;
+        List<Day> result = new ArrayList<>();
+        var db = helper.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT id, day FROM Days",null);
+        try {
+            while (c.moveToNext()) {
+                Day day = new Day(c.getInt(0), c.getString(1), null);
+                result.add(day);
+            }
+        } finally {
+            c.close();
+        }
+        return result;
     }
 
     @Override
     public void insertMeal(Meal meal) {
-
+        long rowId = helper.insertMealData(meal.getProductId(), meal.getProductName(), meal.getCalories(), meal.getProteins(), meal.getCarbs(), meal.getFats(), meal.getQuantity(), meal.getDayId());
+        meal.setId((int)rowId);
     }
 
     @Override
     public void insertDay(Day day) {
-
+        long rowId = helper.insertDayData(day.getDayDate());
+        day.setId((int)rowId);
     }
 
     @Override
